f992d647b7dbd12e10391125ca0fb17de1956b84
eBetcel <emanuelbetcel@gmail.com>
2017-11-10 05:18:26 -0300

    Update imagem.h
    ident and comments

--- imagem.h
+++ imagem.h
@@ -8,20 +8,19 @@
 #include <string.h>
 #include <stdlib.h>
 #endif
-
+ //Define a struct das cores, com 3 inteiros unsigned
 typedef struct{
 	unsigned int r,g,b;
 }Pixel;
-
+//Define a struct para os pixels
 typedef struct {
 	Pixel **p;
 	int width,height,max;
 	char header[2];
 }Imagem;
 
-Imagem rimg(char * name);
-void Grey(Imagem *Foto);
-Imagem *Gauss(Imagem *Foto);
-
-void wimg(FILE  *file, Imagem *img);
+Imagem rimg(char * name); //Função de leitura
+void Grey(Imagem *Foto); //Função para transformar em tons de cinza
+Imagem *Gauss(Imagem *Foto); //Função para o filtro de gauss
+void wimg(FILE  *file, Imagem *img);//Função de escrita 
 #endif
