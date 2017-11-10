4e84632c48dc2cfa861ce4218d9f5671b62d5eae
eBetcel <emanuelbetcel@gmail.com>
2017-11-10 05:21:13 -0300

    Update wimg.c
    ident and comments

--- wimg.c
+++ wimg.c
@@ -2,8 +2,9 @@
 void wimg(FILE *file, Imagem *img)
 {
     int i, j;
-
+//Imprime no arquivo o cabeçalho: header, largura, altura e pxl max
     fprintf(file,"%s\n%d %d\n%d\n",img->header,img->width,img->height,img->max);
+//Imprime no arquivo os pixels
     for (i = 0; i < img->height; ++i)
     {
         for (j = 0; j < img->width; ++j)
