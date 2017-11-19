6302458747017e6e3a209bbc9cc2ebf9fdde5564
maxsnv <max_arthas@hotmail.com>
2017-11-19 18:53:08 -0300

    Update hough.c
    Tinha dado up com modificações ainda nao testadas

--- hough.c
+++ hough.c
@@ -10,7 +10,7 @@ unsigned int **HTransform(Imagem *img)
  double  hough_h = sqrt(w*w + h*h)/2.0;
  double r;
  int  img_d = hough_h *2.0; // de -r para +r; diagonal da imagem
- int  i,j,t,th = 180; //max theta
+ int  i,j,t,th = 360; //max theta
  unsigned int **accu;
  accu = malloc(img_d*sizeof(unsigned int*));
  for (i = 0; i < img_d; ++i) 
@@ -35,7 +35,7 @@ unsigned int **HTransform(Imagem *img)
 	 {
 	  for (t = 0;t < th;t++) 
 	  {
- 	   r = ((j-mid_x)*(cos(rad(t))+(i-mid_y)*sin(rad(t))));
+ 	   r = (j*(cos(rad(t))+i*sin(rad(t))));
 	   accu[r,t]++;
 	  }
 	 }
